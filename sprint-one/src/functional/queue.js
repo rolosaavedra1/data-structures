9a62b120147581d8304f819c9676674a43143480
rolosaavedra1 <rodrigo.saavedra@screendrive.io>
2020-10-07 13:59:31 -0700

    Completed stack and queue constructor function

--- sprint-one/src/functional/queue.js
+++ sprint-one/src/functional/queue.js
@@ -2,17 +2,30 @@ var Queue = function() {
   var someInstance = {};
 
   // Use an object with numeric keys to store values
-  var storage = {};
+  someInstance.storage =  {};
 
   // Implement the methods below
 
   someInstance.enqueue = function(value) {
+    var tempObj = {};
+    for (var i = someInstance.size(); i > 0; i--) {
+      someInstance.storage[i] = someInstance.storage[i-1];
+    }
+    someInstance.storage[0] = value;
   };
 
   someInstance.dequeue = function() {
+    var deleted = someInstance.storage[someInstance.size() - 1];
+    delete someInstance.storage[someInstance.size() - 1];
+    return deleted;
   };
 
   someInstance.size = function() {
+    var counter = 0;
+    while (someInstance.storage[counter]){
+      counter++;
+    }
+    return counter;
   };
 
   return someInstance;
--- sprint-one/src/functional/stack.js
+++ sprint-one/src/functional/stack.js
@@ -1,18 +1,31 @@
 var Stack = function() {
   var someInstance = {};
-
   // Use an object with numeric keys to store values
-  var storage = {};
-
+  someInstance.storage =  {};
   // Implement the methods below
   someInstance.push = function(value) {
+    //run someInstance.size
+    //store new value on obj[size]
+    someInstance.storage[someInstance.size()] = value;
   };
-
   someInstance.pop = function() {
+    //run someInstance.size
+    //delete obj[size-1]
+    var deleted = someInstance.storage[someInstance.size() - 1];
+    delete someInstance.storage[someInstance.size() - 1];
+    return deleted;
   };
-
   someInstance.size = function() {
+    //run a while loop, go through every instance of the storage object
+    //when reach undefined, return counter
+    var counter = 0;
+    while (someInstance.storage[counter]) {
+      counter++;
+    }
+    return counter;
   };
-
   return someInstance;
 };
+
+
+
