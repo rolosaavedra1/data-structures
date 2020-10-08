e37e733cd556a95a270370e80a03d2632bdbdeb5
rolosaavedra1 <rodrigo.saavedra@screendrive.io>
2020-10-08 02:15:19 -0700

    Completed pseudoclassical stack and queue constructor functions

--- sprint-one/src/pseudoclassical/queue.js
+++ sprint-one/src/pseudoclassical/queue.js
@@ -1,6 +1,27 @@
 var Queue = function() {
-  // Hey! Rewrite in the new style. Your code will wind up looking very similar,
-  // but try not not reference your old code in writing the new style.
+  this.storage = {};
+};
+
+Queue.prototype.size = function() {
+  var counter = 0;
+  while (this.storage[counter]){
+    counter++;
+  }
+  return counter;
+};
+
+Queue.prototype.enqueue = function(value) {
+  var tempObj = {};
+  for (var i = this.size(); i > 0; i--) {
+    this.storage[i] = this.storage[i-1];
+  }
+  this.storage[0] = value;
+};
+
+Queue.prototype.dequeue = function() {
+  var deleted = this.storage[this.size() - 1];
+  delete this.storage[this.size() - 1];
+  return deleted;
 };
 
 
--- sprint-one/src/pseudoclassical/stack.js
+++ sprint-one/src/pseudoclassical/stack.js
@@ -1,6 +1,25 @@
 var Stack = function() {
-  // Hey! Rewrite in the new style. Your code will wind up looking very similar,
-  // but try not not reference your old code in writing the new style.
+  this.storage = {};
 };
+
+Stack.prototype.size = function () {
+  var counter = 0;
+  while (this.storage[counter]) {
+    counter++;
+  }
+  return counter;
+};
+
+Stack.prototype.push = function(value) {
+  this.storage[this.size()] = value;
+};
+
+Stack.prototype.pop = function () {
+  var deleted = this.storage[this.size() - 1];
+  delete this.storage[this.size() - 1];
+  return deleted;
+};
+var stack = new Stack;
+
 
 
