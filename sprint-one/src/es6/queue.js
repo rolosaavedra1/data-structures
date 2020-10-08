7a7cb8cf508bfd2e06945a79a275c7be8241b66e
rolosaavedra1 <rodrigo.saavedra@screendrive.io>
2020-10-08 02:28:38 -0700

    Completed ES6 contructor functions and instantiation patters for stack and queue

--- sprint-one/src/es6/queue.js
+++ sprint-one/src/es6/queue.js
@@ -1,7 +1,28 @@
 class Queue {
-  // Hey! Rewrite in the new style. Your code will wind up looking very similar,
-  // but try not not reference your old code in writing the new style.
   constructor() {
+    this.storage = {};
   }
 
+  size() {
+    var counter = 0;
+    while (this.storage[counter]){
+      counter++;
+    }
+    return counter;
+  }
+
+  enqueue(value) {
+    var tempObj = {};
+    for (var i = this.size(); i > 0; i--) {
+      this.storage[i] = this.storage[i-1];
+    }
+    this.storage[0] = value;
+  }
+
+  dequeue() {
+    var deleted = this.storage[this.size() - 1];
+    delete this.storage[this.size() - 1];
+    return deleted;
+  }
 }
+var queue = new Queue;
--- sprint-one/src/es6/stack.js
+++ sprint-one/src/es6/stack.js
@@ -1,7 +1,25 @@
 class Stack {
-  // Hey! Rewrite in the new style. Your code will wind up looking very similar,
-  // but try not not reference your old code in writing the new style.
+
   constructor() {
+    this.storage = {};
   }
 
+  size() {
+    var counter = 0;
+    while (this.storage[counter]) {
+      counter++;
+    }
+    return counter;
+  }
+
+  push(value) {
+    this.storage[this.size()] = value;
+  }
+
+  pop() {
+    var deleted = this.storage[this.size() - 1];
+    delete this.storage[this.size() - 1];
+    return deleted;
+  }
 }
+var stack = new Stack;
