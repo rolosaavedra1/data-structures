90bf03ff8859243a5200031d6b25d2e85b9e6bb2
rolosaavedra1 <rodrigo.saavedra@screendrive.io>
2020-10-08 02:01:43 -0700

    Completed prototype-stack and prototype-queue constructor functions

--- sprint-one/src/prototypal/queue.js
+++ sprint-one/src/prototypal/queue.js
@@ -1,8 +1,33 @@
 var Queue = function() {
-  // Hey! Rewrite in the new style. Your code will wind up looking very similar,
-  // but try not not reference your old code in writing the new style.
+  var newQueue = Object.create(queueMethods);
+  newQueue.storage = {};
+
+  return newQueue;
 };
 
-var queueMethods = {};
+var queueMethods = {
+
+  size: function() {
+    var counter = 0;
+    while (this.storage[counter]){
+      counter++;
+    }
+    return counter;
+  },
+
+  enqueue: function(value) {
+    var tempObj = {};
+    for (var i = this.size(); i > 0; i--) {
+      this.storage[i] = this.storage[i-1];
+    }
+    this.storage[0] = value;
+  },
+
+  dequeue: function() {
+    var deleted = this.storage[this.size() - 1];
+    delete this.storage[this.size() - 1];
+    return deleted;
+  }
+};
 
 
--- sprint-one/src/prototypal/stack.js
+++ sprint-one/src/prototypal/stack.js
@@ -1,8 +1,28 @@
 var Stack = function() {
-  // Hey! Rewrite in the new style. Your code will wind up looking very similar,
-  // but try not not reference your old code in writing the new style.
+  var newStack = Object.create(stackMethods);
+  newStack.storage = {};
+
+  return newStack;
 };
 
-var stackMethods = {};
+var stackMethods = {
+
+  size: function () {
+    var counter = 0;
+    while (this.storage[counter]) {
+      counter++;
+    }
+    return counter;
+  },
+
+  push: function(value) {
+    this.storage[this.size()] = value;
+  },
+  pop: function () {
+    var deleted = this.storage[this.size() - 1];
+    delete this.storage[this.size() - 1];
+    return deleted;
+  }
+};
 
 
